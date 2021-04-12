5c4a88cf740821546bde3557b4cae34f6dba4d59
kpavaday <kpavaday@gmail.com>
2021-04-13 00:43:59 +0200

    Eval + sql

--- PHPWeb/EMPLOYEv1/script.sql
+++ PHPWeb/EMPLOYEv1/script.sql
@@ -1,15 +1,9 @@
-#------------------------------------------------------------
-#        Script MySQL.
-#------------------------------------------------------------
 DROP DATABASE IF EXISTS employe;
 
 CREATE DATABASE IF NOT EXISTS employe;
 
 USE employe;
 
-#------------------------------------------------------------
-# Table: Agence
-#------------------------------------------------------------
 CREATE TABLE Agence(
     idAgence Int NOT NULL Auto_increment PRIMARY KEY,
     nomAgence Varchar (200) NOT NULL,
@@ -23,23 +17,25 @@ CREATE TABLE Employe(
     idEmploye Int NOT NULL Auto_increment PRIMARY KEY,
     nomEmploye Varchar (200) NOT NULL,
     prenomEmploye Varchar (200) NOT NULL,
-    dateEmbauche Date (20) NOT NULL?
+    dateEmbauche Date (20) NOT NULL,
     fonction Varchar (200) NOT NULL,
     salaire Float (20) NOT NULL,
     serviceEmploye Varchar (200) NOT NULL,
     nbreEnfants Int (10) NOT NULL,
-)
+    idAgence INT NOT NULL
+) ENGINE = InnoDB;
 
 CREATE TABLE Enfant(
     idEnfant Int NOT NULL Auto_increment PRIMARY KEY,
     nomEnfant Varchar (200) NOT NULL,
     prenomEnfant Varchar (200) NOT NULL,
-    ageEnfant Float (10) NOT NULL
-)
+    ageEnfant Float (10) NOT NULL,
+    idEmploye INT NOT NULL
+) ENGINE = InnoDB;
 
 ALTER TABLE Enfant
 ADD
-CONSTRAINT fk_enfant_employe FOREIGN KEY (idEmploye) REFERENCES Employe(idEmploye);
+CONSTRAINT FK_enfant_employe FOREIGN KEY (idEmploye) REFERENCES Employe(idEmploye);
 
 ALTER TABLE Employe
 ADD
