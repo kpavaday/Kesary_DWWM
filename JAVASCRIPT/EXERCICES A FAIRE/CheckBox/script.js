992ed1e9ac9f1dadaeb7d45ec45cc96862f93653
kpavaday <kpavaday@gmail.com>
2021-05-04 17:23:11 +0200

    maj

---
+++ JAVASCRIPT/EXERCICES A FAIRE/CheckBox/script.js
@@ -0,0 +1,5 @@
+const checkbox = document.querySelectorAll('input[name="couleur"] :checked');
+
+//On recherche les valeurs de chaque checkBox
+let couleurs =[];
+
--- JAVASCRIPT/EXERCICES A FAIRE/JS07/script.js
+++ JAVASCRIPT/EXERCICES A FAIRE/JS07/script.js
@@ -1,2 +1,25 @@
+
 let btnAjout = document.querySelector('#ajout');
-let btnSuppr = document.querySelector('#suppr');
+let suppression = document.querySelector('li');
+// let btnSuppr = document.querySelector('#suppr');
+
+//Ajouter un dessert
+btnAjout.addEventListener('click',function(){
+    var dessert = prompt("Entrez le dessert de votre choix");
+    if(dessert != null){
+        document.getElementById("ici").innerHTML = dessert ;
+    }
+});
+
+//Supprimer un dessert
+suppression.addEventListener('click', suppr);
+
+    
+function suppr(){
+    var cible = document.getElementById("idDessert");
+    cible.remove();
+   
+}
+
+
+
