183c29ac3130e26afbf732dabf0ff4a358ccbfc0
kpavaday <kpavaday@gmail.com>
2021-05-04 12:15:46 +0200

    12:15:45,52

--- JAVASCRIPT/EXERCICES A FAIRE/JS06/script.js
+++ JAVASCRIPT/EXERCICES A FAIRE/JS06/script.js
@@ -1,15 +1,21 @@
 
+//Changer la couleur du H1
 var leTitre = document.querySelector('h1');
 leTitre.addEventListener('click',function(){
     this.classList.toggle('red')
 
 });
 
-
-
-
-
-
-   
-
+//Changer la couleur de tous les paragraphes en cliquant dessus
+var paraf = document.querySelectorAll('p');
+for(var i=0;i<paraf.length;i++){
+    paraf[i].addEventListener('click', function(){
+        for(var i=0;i<paraf.length;i++){
+            if(paraf[i].style.color == "green"){
+                paraf[i].style.color="yellow";
+            }else{
+                paraf[i].style.color='green';
+            };}
+    }
+)};
 
