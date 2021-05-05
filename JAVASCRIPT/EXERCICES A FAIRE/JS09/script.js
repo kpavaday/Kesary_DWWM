a10e89c79c85f113df6eb30676986f7eed598677
kpavaday <kpavaday@gmail.com>
2021-05-05 12:15:46 +0200

    12:15:45,50

---
+++ JAVASCRIPT/EXERCICES A FAIRE/JS09/script.js
@@ -0,0 +1,25 @@
+let seDeplacerDroite = document.addEventListener(keyDown,function(e){
+    if(e.arrowRight && e.keyCode ==   )
+
+});
+
+
+
+
+// let seDeplacerGauche = document.addEventListener(keyDown,versLaGauche);
+// let seDeplacerHaut = document.addEventListener(keyDown,versLeHaut);
+// let seDePlacerBas = document.addEventListener(keyDown,verLeBas);
+
+
+// function versLaDroite(){
+
+// }
+// function versLaGauche(){
+
+// }
+// function versLeHaut(){
+
+// }
+// function verLeBas(){
+
+// }
